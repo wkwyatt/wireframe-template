75c33d377d537c526b24670fae200d36e979a482
wkwyatt <wkwyatt@gmail.com>
2015-10-23 15:15:59 -0400

    Initial Commit
    bug with css of content

---
+++ assets/compass/config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+
+#Folder settings
+relative_assets = true		#because we are not working from the root
+css_dir = "../css"			#where the CSS will be saved
+sass_dir = "sass"			#where our .scss files are
+images_dir = "../images"	#the folder with your images
+
+# You can select your preferred output style here (can be overriden on command line):
+output_style = :expanded 	#After dev :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
+
+#Obviously
+preferred_syntax = :scss
