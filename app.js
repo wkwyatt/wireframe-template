c49695179a1466a0dddf996d2d1df2437a8f6d6f
wkwyatt <wkwyatt1@gmail.com>
2016-01-03 17:35:06 -0500

    version commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
-connect().use(serveStatic(__dirname)).listen(8000);
+// connect().use(serveStatic(__dirname)).listen(8000);
